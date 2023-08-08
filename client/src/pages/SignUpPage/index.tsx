284e923ea9cf65b973aebc606cadc14aa5bc66d5
Danny O'Neal <danny.e.oneal@gmail.com>
2023-08-08 13:21:49 -0700

    should trigger an action

--- client/src/pages/SignUpPage/index.tsx
+++ client/src/pages/SignUpPage/index.tsx
@@ -102,7 +102,7 @@ const SignUpPage = () => {
               style={{ color: "crimson", fontStyle: "italic" }}
             >
               Official launch in fall 2023, beta user onboarding begins
-              mid-June.
+              mid-June. October 20
             </h6>
 
             <Loader
